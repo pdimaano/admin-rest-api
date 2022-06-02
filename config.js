4476b3d440481a077970e152f78a35825f50cea9
Philip Dimaano <pdimaano7@gmail.com>
2022-06-01 18:02:27 -0700

    initial commit

---
+++ config.js
@@ -0,0 +1,8 @@
+const config = {
+    devAdminURL: "http://localhost:3001",
+    prodAdminURL: "https://www.admin.pdimablog.xyz",
+    devFrontendWebsiteURL: "http://localhost:3000",
+    prodFrontendWebsiteURL: "https://www.pdimablog.xyz"
+}
+
+module.exports = config
